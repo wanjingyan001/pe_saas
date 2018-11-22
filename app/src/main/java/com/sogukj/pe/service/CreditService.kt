b8c20a3e0ec039525fe29fce216c7a6203ea87cf
admin <wanjy@sogukj.com>
2018-11-22 17:40:31 +0800

    征信请求地址修改

--- app/src/main/java/com/sogukj/pe/service/CreditService.kt
+++ app/src/main/java/com/sogukj/pe/service/CreditService.kt
@@ -1,11 +1,9 @@
 package com.sogukj.pe.service
 
+import com.sogukj.pe.Consts
 import com.sogukj.pe.bean.*
 import io.reactivex.Observable
-import retrofit2.http.Body
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.POST
+import retrofit2.http.*
 
 /**
  * Created by admin on 2018/5/23.
@@ -14,6 +12,7 @@ interface CreditService {
     /**
      * 征信开始界面接口
      */
+    @Headers(value = ["Domain-Name: ${Consts.CREDIT_COLLECTION}"])
     @FormUrlEncoded
     @POST("/api/Credit/showCreditInfo")
     fun showCreditInfo(@Field("company_id") company_id: Int): Observable<Payload<CreditInfo>>
@@ -21,6 +20,7 @@ interface CreditService {
     /**
      * 征信开始界面接口(改版)
      */
+    @Headers(value = ["Domain-Name: ${Consts.CREDIT_COLLECTION}"])
     @FormUrlEncoded
     @POST("/api/Credit/creditList")
     fun showCreditList(@Field("company_id") company_id: Int? = null,
@@ -31,12 +31,14 @@ interface CreditService {
     /**
      * 征信-敏感信息
      */
+    @Headers(value = ["Domain-Name: ${Consts.CREDIT_COLLECTION}"])
     @POST("/api/Credit/sensitiveData")
     fun sensitiveData(@Body map: HashMap<String, Any>): Observable<Payload<SensitiveInfo>>
 
     /**
      * 征信-敏感信息(修改)
      */
+    @Headers(value = ["Domain-Name: ${Consts.CREDIT_COLLECTION}"])
     @FormUrlEncoded
     @POST("/api/Credit/sensitiveInfo")
     fun sensitiveInfo(@Field("idCard") idCard: String): Observable<Payload<SensitiveInfo>>
@@ -44,6 +46,7 @@ interface CreditService {
     /**
      * 涉诉信息相关
      */
+    @Headers(value = ["Domain-Name: ${Consts.CREDIT_COLLECTION}"])
     @FormUrlEncoded
     @POST("/api/Credit/declarationList")
     fun declarationList(@Field("id") id: Int,
@@ -54,9 +57,11 @@ interface CreditService {
      * 一键查询
      */
 //    @FormUrlEncoded
+    @Headers(value = ["Domain-Name: ${Consts.CREDIT_COLLECTION}"])
     @POST("/api/Credit/surveyCredit")
     fun queryCreditInfo(@Body map: HashMap<String, Any>): Observable<Payload<CreditInfo.Item>>
 
+    @Headers(value = ["Domain-Name: ${Consts.CREDIT_COLLECTION}"])
     @FormUrlEncoded
     @POST("/api/Credit/deleteCredit")
     fun deleteCredit(@Field("id") id: Int): Observable<Payload<Any>>
@@ -65,6 +70,7 @@ interface CreditService {
     /**
      * 百融征信
      */
+    @Headers(value = ["Domain-Name: ${Consts.CREDIT_COLLECTION}"])
     @FormUrlEncoded
     @POST("/api/Credit/queryPersonalCre")
     fun HundredCredit(@Field("name") name: String,
@@ -75,6 +81,7 @@ interface CreditService {
     /**
      * 百融征信查询记录
      */
+    @Headers(value = ["Domain-Name: ${Consts.CREDIT_COLLECTION}"])
     @FormUrlEncoded
     @POST("/api/Credit/PersonalCreList")
     fun getPersonalCreList(@Field("offset") offset: Int = 0,
@@ -85,6 +92,7 @@ interface CreditService {
     /**
      * 个人资质-详情
      */
+    @Headers(value = ["Domain-Name: ${Consts.CREDIT_COLLECTION}"])
     @FormUrlEncoded
     @POST("/api/Credit/PersonalCreInfo")
     fun hundredCreditDetail(@Field("idCard") idCard: String): Observable<Payload<NewCreditInfo>>
